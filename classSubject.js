cdae044d43bf74d2e84afcd51a0032288099b8d9
lemfritz11 <aintdaiki@gmail.com>
2024-11-04 19:27:31 +0800

    initial commit with clssSubject.js

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures and Algorithms";
+let classSchedule = "Monday, and Friday, 8:00 AM - 9:00 AM";
+let classroom = "Computer Lab A";
+let classInstructor = "Vitruvius John Barayuga";
+let studentName = "Lem Fritz Refuerzo";
+
+console.log(studentName + " is currently enrolled in " + subjectTitle + " with a class schedule of " + classSchedule + " at room " + classroom + ". The instructor for the subject is " + classInstructor + ".");
